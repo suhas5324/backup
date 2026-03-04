32c847a32e59db83662e215e3e8099dbf853008b
Suhas s <suhas.s718204@gmail.com>
2026-03-04 11:03:25 +0530

    implement movie management SPs, analytical queries, and indexing optimizations

---
+++ SQL/Milestone-3/SQL_QUERIES.sql
@@ -0,0 +1,114 @@
+-- 1.Write a query to get the age of the Actors in Days(Number of days).
+SELECT ActorName, 
+        CASE 
+            WHEN DateOfBirth IS NOT NULL 
+            THEN DATEDIFF(DAY, DateOfBirth, GETDATE()) 
+            ELSE NULL
+        END AS AgeInTotalDays
+FROM Foundation.Actors
+
+
+-- 2.Write a query to get the list of Actors who have worked with a given producer X.
+SELECT DISTINCT A.ActorName
+FROM Foundation.Actors A
+ JOIN Foundation.Actors_Movie AM
+    ON A.Id = AM.ActorId
+ JOIN Foundation.Movies M
+    ON AM.MovieId = M.Id
+ JOIN Foundation.Producers P
+    ON M.ProducerId = P.Id
+WHERE P.ProducerName = 'X'
+
+
+-- 3.Write a query to get the list of actors who have acted together in two or more movies.
+SELECT 
+    A1.ActorName AS Actor1,
+    A2.ActorName AS Actor2,
+    COUNT(*) AS MoviesTogether
+FROM Foundation.Actors_Movie AM1
+JOIN Foundation.Actors_Movie AM2
+    ON AM1.MovieId = AM2.MovieId
+    AND AM1.ActorId < AM2.ActorId
+JOIN Foundation.Actors A1
+    ON AM1.ActorId = A1.Id
+JOIN Foundation.Actors A2
+    ON AM2.ActorId = A2.Id
+GROUP BY A1.ActorName, A2.ActorName
+HAVING COUNT(*) >= 2
+
+
+-- 4.Write a query to get the youngest actor.
+SELECT *
+FROM Foundation.Actors
+WHERE DateOfBirth = 
+(
+    SELECT MAX(DateOfBirth)
+    FROM Foundation.Actors
+);
+
+
+-- 5.Write a query to get the actors who have never worked together.
+SELECT 
+    A1.ActorName AS Actor1,
+    A2.ActorName AS Actor2
+FROM Foundation.Actors A1
+JOIN Foundation.Actors A2
+    ON A1.Id < A2.Id
+WHERE NOT EXISTS
+(
+    SELECT 1
+    FROM Foundation.Actors_Movie AM1
+    JOIN Foundation.Actors_Movie AM2
+        ON AM1.MovieId = AM2.MovieId
+    WHERE AM1.ActorId = A1.Id
+      AND AM2.ActorId = A2.Id
+)
+
+
+-- 6.Write a query to get the number of movies in each language.
+SELECT 
+    MovieLanguage,
+    COUNT(*) AS MovieCountByLanguage
+FROM Foundation.Movies
+GROUP BY MovieLanguage
+
+
+-- 7.Write a query to get me the total profit of all the movies in each language separately.
+SELECT 
+    MovieLanguage,
+    SUM(Profit) AS TotalProfitByLanguage
+FROM Foundation.Movies
+GROUP BY MovieLanguage
+
+
+-- 8.Write a query to get the total profit of movies which have actor X in each language.
+SELECT 
+    M.MovieLanguage,
+    SUM(M.Profit) AS TotalProfit
+FROM Foundation.Movies M
+JOIN Foundation.Actors_Movie AM 
+    ON M.Id = AM.MovieId
+JOIN Foundation.Actors A 
+    ON AM.ActorId = A.Id
+WHERE A.ActorName = 'X'
+GROUP BY M.MovieLanguage
+
+
+-- 9.Write a query to get the Total profit by year of release and language
+SELECT 
+    YearOfRelease,
+    MovieLanguage,
+    SUM(Profit) AS TotalProfitByYearAndLanguage
+FROM Foundation.Movies
+GROUP BY YearOfRelease, MovieLanguage
+
+
+-- 10.Write a query to get number of movies in each language produced by each producer
+SELECT 
+    P.ProducerName,
+    M.MovieLanguage,
+    COUNT(*) AS MovieCount
+FROM Foundation.Movies M
+JOIN Foundation.Producers P
+    ON M.ProducerId = P.Id
+GROUP BY P.ProducerName, M.MovieLanguage
---
+++ SQL/Milestone-3/SQL_STORED_PROCEDURE.sql
@@ -0,0 +1,82 @@
+-- Question 1
+CREATE PROCEDURE Foundation.usp_InsertMovie
+(
+    @Name VARCHAR(200),
+    @YearOfRelease INT,
+    @Plot VARCHAR(500),
+    @PosterImagePath VARCHAR(500),
+    @ProducerId INT,
+    @ActorIds VARCHAR(100)  
+)
+AS
+BEGIN
+
+    DECLARE @MovieId INT;
+
+    INSERT INTO Foundation.Movies (MovieName, YearOfRelease, Plot, PosterImagePath, ProducerId)
+    VALUES (@Name, @YearOfRelease, @Plot, @PosterImagePath, @ProducerId);
+
+    SET @MovieId = SCOPE_IDENTITY();
+
+    INSERT INTO Foundation.Actors_Movie (ActorId, MovieId)
+    SELECT CAST(value AS INT), @MovieId
+    FROM STRING_SPLIT(@ActorIds, ',');
+
+END;
+
+-- Question 2
+
+CREATE PROCEDURE Foundation.usp_DeleteMovie
+(
+    @MovieId INT
+)
+AS
+BEGIN
+
+    DELETE FROM Foundation.Actors_Movie
+    WHERE MovieId = @MovieId;
+
+    DELETE FROM Foundation.Movies
+    WHERE Id = @MovieId;
+
+END;
+
+-- Question 3
+
+CREATE PROCEDURE Foundation.usp_DeleteProducer
+(
+    @ProducerId INT
+)
+AS
+BEGIN
+
+    DELETE AM
+    FROM Foundation.Actors_Movie AM
+    INNER JOIN Foundation.Movies M
+        ON AM.MovieId = M.Id
+    WHERE M.ProducerId = @ProducerId;
+
+    DELETE FROM Foundation.Movies
+    WHERE ProducerId = @ProducerId;
+
+    DELETE FROM Foundation.Producers
+    WHERE Id = @ProducerId;
+
+END;
+
+-- Question 4
+
+CREATE PROCEDURE Foundation.usp_DeleteActor
+(
+    @ActorId INT
+)
+AS
+BEGIN
+
+    DELETE FROM Foundation.Actors_Movie
+    WHERE ActorId = @ActorId;
+
+    DELETE FROM Foundation.Actors
+    WHERE Id = @ActorId;
+
+END;
